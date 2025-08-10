46aeddee54562b2211ee2912562ebce686e520f4
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-08-10 03:49:37 +0100

    fix: update types and build without errors

--- buyAgain_backend/src/types/index.d.ts
+++ buyAgain_backend/src/types/index.d.ts
@@ -3,8 +3,13 @@ import { FilterQuery } from 'mongoose';
 import { IUser } from '../models/userModel';
 import { IProduct } from './../models/productModel';
 
-// Keep all default Request props and add your custom ones
-export interface CustomRequest extends Request<Record<string, any>, any, any> {
+// Keep all default Express.Request props and add ours
+export interface CustomRequest<
+  P = any,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any,
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
   user?: IUser;
   userFilter?: FilterQuery<any>;
   product?: IProduct;
