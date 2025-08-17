2d8748c0f98faa2c23dbd82230d79965be8ab3ad
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-08-17 14:04:50 +0100

    Fix (frontend - alert): covers the home page.

--- buyAgain_frontend/src/utils/alert.ts
+++ buyAgain_frontend/src/utils/alert.ts
@@ -13,12 +13,13 @@ export const showAlert = (
   hideAlert();
 
   // Determine styles based on alert type
-  const baseClasses = 'fixed top-4 inset-x-0 flex justify-center z-50';
-  const alertClasses = `px-4 py-3 rounded-lg shadow-lg transition-transform duration-200 cursor-default max-w-sm w-full mx-auto`;
+  const baseClasses =
+    'fixed top-0 inset-x-0 left-1/2 transform -translate-x-1/2 z-50';
+  const alertClasses = `px-4 py-3 rounded-sm shadow-lg transition-transform duration-200 cursor-default max-w-sm w-full mx-auto`;
   const typeClasses =
     type === 'success'
-      ? 'bg-pink-200 border-2 border-green-500 text-green-700'
-      : 'bg-pink-200 border-2 border-red-500 text-red-700';
+      ? 'bg-green-200 border-2 border-green-500 text-green-700'
+      : 'bg-green-200 border-2 border-red-500 text-red-700';
 
   const combinedClasses = `${baseClasses} ${alertClasses} ${typeClasses}`;
 
